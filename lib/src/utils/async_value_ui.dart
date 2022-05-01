dbabea675e632ca2bec96940dfae1852060dfe8b
Theo Franco <theo@entomoio.com>
2022-05-01 00:40:22 -0300

    error alert translation

--- lib/src/utils/async_value_ui.dart
+++ lib/src/utils/async_value_ui.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:localization_ecommerce/src/common_widgets/alert_dialogs.dart';
-import 'package:localization_ecommerce/src/localization/string_hardcoded.dart';
+import 'package:localization_ecommerce/src/localization/app_localizations_context.dart';
 
 extension AsyncValueUI on AsyncValue {
   void showAlertDialogOnError(BuildContext context) {
     if (!isRefreshing && hasError) {
       showExceptionAlertDialog(
         context: context,
-        title: 'Error'.hardcoded,
+        title: context.loc.anErrorOccurred,
         exception: error,
       );
     }
